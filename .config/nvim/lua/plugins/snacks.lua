69776bec6253ea462ba181a8c0f64d1ce812a499
Udit Mehrotra <udmehrot@cisco.com>
2025-03-18 20:46:36 -0400

    update

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -28,5 +28,10 @@ return {
         },
       },
     },
+    image = {
+      -- your image configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
   },
 }
